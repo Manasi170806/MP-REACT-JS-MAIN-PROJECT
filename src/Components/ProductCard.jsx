dd61b6d5cd7c0479814071bfdabec8a2d691fb6d
Manasi170806 <manasipatel17082006@gmail.com>
2025-07-31 13:50:51 +0530

    pending

--- src/Components/ProductCard.jsx
+++ src/Components/ProductCard.jsx
@@ -3,22 +3,18 @@ import { Card, Button } from 'react-bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 
-
-
-
-
 const ProductCard = ({ id, title, price, description, category, image, rating }) => {
   return (
     <div className='product-card'>
-        <Card>
+        <Card className='custom-card'>
             <Card.Img variant="top" src={image} />
             <Card.Body>
-                <Card.Title>{title}</Card.Title>
+                <Card.Title>{title.substring(0, 20)}...</Card.Title>
                 <Card.Text>
-                    {price}
+                    <b>Price</b> : {price}
                 </Card.Text>
                 <Button variant="primary">Add to Cart</Button>
-        </Card.Body>
+            </Card.Body>
         </Card>
     </div>
   )
